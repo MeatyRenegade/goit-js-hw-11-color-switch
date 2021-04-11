3249db2da30e0d53fb0c503cc6968864fad17dd4
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-04-11 15:46:08 +0300

    Добавлена реализация переключения цвета по нажатию

---
+++ index.js
@@ -0,0 +1,38 @@
+const colors = [
+  "#FFFFFF",
+  "#2196F3",
+  "#4CAF50",
+  "#FF9800",
+  "#009688",
+  "#795548",
+];
+
+const bodyRef = document.querySelector("body");
+const buttonStartRef = document.querySelector('button[data-action="start"]');
+const buttonStopRef = document.querySelector('button[data-action="stop"]');
+
+buttonStartRef.addEventListener("click", changeColorStart);
+buttonStopRef.addEventListener("click", changeColorStop);
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+let newColor = null;
+
+function changeColorStart() {
+  buttonStartRef.disabled = true;
+
+  newColor = setInterval(() => {
+    const randomNumber = randomIntegerFromInterval(0, colors.length - 1);
+    bodyRef.style.backgroundColor = colors[randomNumber];
+  }, 1000);
+
+  return newColor;
+}
+
+function changeColorStop() {
+  buttonStartRef.disabled = false;
+
+  clearInterval(newColor);
+}
